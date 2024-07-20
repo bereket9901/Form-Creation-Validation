ebd18a8066f3a6f7882b1b31c8f790213fb5800e
Bereket Yimer <bekym0242@gmail.com>
2024-07-20 15:52:35 +0300

    add form

---
+++ script.js
@@ -0,0 +1,36 @@
+const form = document.getElementById("registration-form");
+const feedbackDiv = document.getElementById("form-feedback");
+form.addEventListener('click', function (event) {
+    event.preventDefault();
+    const username = document.getElementById("username");
+    const email = document.getElementById("email");
+    const password = document.getElementById("password");
+
+    const isValid = true;
+    var messages = [];
+
+    if (username.length < 3) {
+
+        isValid = false;
+        messages.push("Username must be at least 3 characters long");
+    }
+    if (!(email.includes(".") && username.email.includes("@"))) {
+        isValid = false;
+        messages.push("Email must be a valid email address");
+    }
+    if (password.length < 8) {
+
+        isValid = false;
+        messages.push("Password must be at least 8 character")
+    }
+    feedbackDiv.style.display = "block"
+    if (isValid) {
+        feedbackDiv.textContent = "Registration successful";
+        feedbackDiv.style.color = "#28a745"
+    }
+    if (!isValid) {
+        feedbackDiv.innerHTML = messages.join(<br />);
+        feedbackDiv.style.color = "#dc3545"
+    }
+})
+
