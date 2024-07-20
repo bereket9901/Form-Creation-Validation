3b944942dc50149ee2331db40ab2201eb4ae2f58
Bereket Yimer <bekym0242@gmail.com>
2024-07-20 15:56:43 +0300

    add DOMContentLoaded

--- script.js
+++ script.js
@@ -1,36 +1,40 @@
-const form = document.getElementById("registration-form");
-const feedbackDiv = document.getElementById("form-feedback");
-form.addEventListener('click', function (event) {
-    event.preventDefault();
-    const username = document.getElementById("username");
-    const email = document.getElementById("email");
-    const password = document.getElementById("password");
 
-    const isValid = true;
-    var messages = [];
+document.addEventListener(
+    "DOMContentLoaded", function () {
+        const form = document.getElementById("registration-form");
+        const feedbackDiv = document.getElementById("form-feedback");
+        form.addEventListener('click', function (event) {
+            event.preventDefault();
+            const username = document.getElementById("username");
+            const email = document.getElementById("email");
+            const password = document.getElementById("password");
 
-    if (username.length < 3) {
+            const isValid = true;
+            var messages = [];
 
-        isValid = false;
-        messages.push("Username must be at least 3 characters long");
-    }
-    if (!(email.includes(".") && username.email.includes("@"))) {
-        isValid = false;
-        messages.push("Email must be a valid email address");
-    }
-    if (password.length < 8) {
+            if (username.length < 3) {
 
-        isValid = false;
-        messages.push("Password must be at least 8 character")
-    }
-    feedbackDiv.style.display = "block"
-    if (isValid) {
-        feedbackDiv.textContent = "Registration successful";
-        feedbackDiv.style.color = "#28a745"
-    }
-    if (!isValid) {
-        feedbackDiv.innerHTML = messages.join(<br />);
-        feedbackDiv.style.color = "#dc3545"
-    }
-})
+                isValid = false;
+                messages.push("Username must be at least 3 characters long");
+            }
+            if (!(email.includes(".") && username.email.includes("@"))) {
+                isValid = false;
+                messages.push("Email must be a valid email address");
+            }
+            if (password.length < 8) {
 
+                isValid = false;
+                messages.push("Password must be at least 8 character")
+            }
+            feedbackDiv.style.display = "block"
+            if (isValid) {
+                feedbackDiv.textContent = "Registration successful";
+                feedbackDiv.style.color = "#28a745"
+            }
+            if (!isValid) {
+                feedbackDiv.innerHTML = messages.join(<br />);
+                feedbackDiv.style.color = "#dc3545"
+            }
+        })
+    }
+)
